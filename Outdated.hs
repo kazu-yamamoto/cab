c8a99db6ad5fc56de52c4c96cb97cb332b5786de
Kazu Yamamoto <kazu@iij.ad.jp>
2011-03-08 18:35:05 +0900

    outdated now displays user only.

--- Outdated.hs
+++ Outdated.hs
@@ -8,34 +8,49 @@ import Data.Attoparsec.Enumerator
 import Data.ByteString (ByteString)
 import Data.Enumerator (Iteratee, run_, ($$))
 import Data.Enumerator.Binary (enumHandle)
-import System.Process
+import qualified Data.Map as M
+import Data.Maybe
+import Distribution.InstalledPackageInfo
+import Distribution.InstalledPackageInfo.Binary
+import Distribution.Package
+import System.FilePath
 import System.IO
+import System.Process
 import Types
 
 outdated :: FunctionCommand
 outdated _ _ _ = do
+    olds <- infoFromCommand "cabal list --installed" cabalListParser
+            >>= userOnly
+    mapM_ printOld olds
+
+----------------------------------------------------------------
+
+infoFromCommand :: String -> Iteratee ByteString IO a -> IO a
+infoFromCommand shellCommand parser = do
     (Nothing, Just hdl, Nothing, _) <- createProcess proSpec
     hSetEncoding hdl latin1
-    olds <- run_ (enumHandle 4096 hdl $$ outdatedParser)
-    mapM_ printOld olds
+    run_ (enumHandle 4096 hdl $$ parser)
   where
     proSpec = CreateProcess {
-        cmdspec = ShellCommand "cabal list --installed"
+        cmdspec = ShellCommand shellCommand
       , cwd = Nothing
       , env = Nothing
       , std_in = Inherit
       , std_out = CreatePipe
       , std_err = Inherit
       , close_fds = True
-}
+      }
+
+----------------------------------------------------------------
 
 data OldPkg = OldPkg String String String deriving Show
 
 printOld :: OldPkg -> IO ()
 printOld (OldPkg name cur new) = putStrLn $ name ++ " " ++ cur ++ " < " ++ new
 
-outdatedParser :: Iteratee ByteString IO [OldPkg]
-outdatedParser = iterParser oldpkgs
+cabalListParser :: Iteratee ByteString IO [OldPkg]
+cabalListParser = iterParser oldpkgs
 
 oldpkgs :: Parser [OldPkg]
 oldpkgs = filter (\(OldPkg _ x y) -> x /= y) <$> many1 oldpkg
@@ -50,9 +65,40 @@ oldpkg = do
     endOfLine
     return $ OldPkg name cur new
   where
-    nonEols = many1 $ satisfy (notInClass "\n")
     skip = many1 nonEols *> endOfLine
     synpsis = string "    Synopsis:" *> nonEols *> endOfLine *> more
           <|> return ()
       where
         more = () <$ many (string "     " *> nonEols *> endOfLine)
+
+nonEols :: Parser String
+nonEols = many1 $ satisfy (notInClass "\n")
+
+----------------------------------------------------------------
+
+type PackageInfo = InstalledPackageInfo_ String
+
+getUserPackageInfo :: FilePath -> IO [PackageInfo]
+getUserPackageInfo file = readBinPackageDB file :: IO [PackageInfo]
+
+getUserCacheFilePath :: IO FilePath
+getUserCacheFilePath =
+    (</> "package.cache") <$> infoFromCommand "ghc-pkg list" ghcPkgListParser
+
+ghcPkgListParser :: Iteratee ByteString IO FilePath
+ghcPkgListParser = iterParser $ do
+    nonEols *> endOfLine
+    many1 (char ' ' *> nonEols *> endOfLine)
+    endOfLine
+    nonColons <* char ':' <* endOfLine
+  where
+    nonColons = many1 $ satisfy (notInClass ":\n")
+
+userOnly :: [OldPkg] -> IO [OldPkg]
+userOnly olds = do
+    m <- toMap <$> (getUserCacheFilePath >>= getUserPackageInfo)
+    return $ filter (\(OldPkg x _ _) -> isJust (M.lookup x m)) olds
+  where
+    toMap ps = M.fromList $ zip (map toString ps) ps
+    toString = fromPackageName . pkgName . sourcePackageId
+    fromPackageName (PackageName x) = x
