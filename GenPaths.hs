76cccce53d1ebbcae31988cfc57105b4a5712c5c
Kazu Yamamoto <kazu@iij.ad.jp>
2012-02-08 14:15:47 +0900

    Using Version.

--- GenPaths.hs
+++ GenPaths.hs
@@ -5,14 +5,13 @@ module GenPaths (genPaths) where
 import Control.Applicative
 import Control.Exception
 import Control.Monad
-import Data.List (intercalate, isSuffixOf)
+import Data.List (isSuffixOf)
 import Distribution.Package
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parse (readPackageDescription)
 import Distribution.Verbosity (silent)
 import Distribution.Version
 import System.Directory
-import System.IO
 
 genPaths :: IO ()
 genPaths = do
@@ -24,20 +23,19 @@ genPaths = do
                       ++ "import Data.Version\n"
                       ++ "\n"
                       ++ "version :: Version\n"
-                      ++ "version = Version [" ++ ver ++ "] []\n"
+                      ++ "version = " ++ show ver ++ "\n"
   where
     check file = do
         exist <- doesFileExist file
-        unless exist $ hPutStrLn stderr $ file ++ " already exists"
+        when exist . throwIO . userError $ file ++ " already exists"
 
-getNameVersion :: FilePath -> IO (String,String)
+getNameVersion :: FilePath -> IO (String,Version)
 getNameVersion file = do
     desc <- readPackageDescription silent file
     let pkg = package . packageDescription $ desc
         PackageName nm = pkgName pkg
         name = map (trans '-' '_') nm
-        ver = versionBranch . pkgVersion $ pkg
-        version = intercalate "." $ map show ver
+        version = pkgVersion pkg
     return (name, version)
   where
     trans c1 c2 c
