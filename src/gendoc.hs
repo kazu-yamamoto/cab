dd8c7f4818cc064248055bc3e5b549ba10ab9b01
Kazu Yamamoto <kazu@iij.ad.jp>
2013-09-24 14:53:03 +0900

    adding gendoc.hs.

---
+++ src/gendoc.hs
@@ -0,0 +1,43 @@
+-- runghc -- -package-db --ghc-arg=../.cabal-sandbox/i386-osx-ghc-7.6.3-packages.conf.d gendoc.hs
+
+module Main where
+
+import Commands
+import Options
+import Types
+import Help
+
+main :: IO ()
+main = do
+    putStrLn "The following commands are provided:"
+    putStr "\n"
+    putStrLn "?cab"
+    putStrLn "!Display the help message."
+    mapM_ prCmdSpec commandDB
+    putStr "\n"
+    putStrLn "The following options are provided:"
+    putStr "\n"
+    mapM_ prOptSpec optionDB
+
+prCmdSpec :: CommandSpec -> IO ()
+prCmdSpec cmdspec = do
+    putStrLn $ "?cab " ++ escape usage
+    putStrLn $ "!" ++ doc ++ "." ++ aliases
+  where
+    (usage,doc,alias) = usageDocAlias cmdspec
+    aliases
+      | alias == "" = ""
+      | otherwise   = "\\<br /\\>Command aliases: " ++ escape alias
+
+prOptSpec :: OptionSpec -> IO ()
+prOptSpec spec = do
+    putStrLn $ "?" ++ option
+    putStrLn $ "!" ++ doc
+  where
+    (option, doc) = optionDoc spec
+
+escape :: String -> String
+escape [] = []
+escape ('[':rest) = "\\[" ++ escape rest
+escape (']':rest) = "\\]" ++ escape rest
+escape (c:rest)   = c : escape rest
